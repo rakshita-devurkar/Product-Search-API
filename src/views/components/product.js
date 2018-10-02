249216690dbb7b76a938f32b81bab879562c29c0
Rakshita Devurkar <rdevurk1@binghamton.edu>
2018-10-02 11:31:16 -0400

    Removed comments

--- src/views/components/product.js
+++ src/views/components/product.js
@@ -2,25 +2,6 @@ import React from 'react';
 import './style.css';
 
 export default class Product extends React.Component {
-	// renderImage(imageUrl) {
-	// 	return (
-	// 		<div>
-	// 		<img src = {imageUrl} />
-	// 		</div>
-	// 	);
-	// }
-
-	// render() {
-	// 	return (
-	// 		<div className="gallery">
-	// 		<div className="images">
-	// 			{this.props.allProducts.map(imageUrl => this.renderImage(imageUrl))}
-
-	// 		</div>
-	// 		</div>
-	// 	)
-	// }
-
 	render() {
 		let imageUrl = this.props.imageUrl;
 		let name = this.props.name;
@@ -37,8 +18,3 @@ export default class Product extends React.Component {
 		)
 	}
 }
-
-// Product.propTypes = {
-// 	imageUrls : React.PropTypes.arrayOf(React.PropTypes.string).isRequired
-// };
-
