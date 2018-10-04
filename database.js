bbf4e9de31e92de1eef67ff51fd0308be4e21cc2
Rakshita Devurkar <rdevurk1@binghamton.edu>
2018-10-04 13:12:16 -0400

    Delete existing db data before starting server

--- database.js
+++ database.js
@@ -5,8 +5,14 @@ const db = Mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error'));
 //Do Boom error handling here
-db.once('open', function callback() {
-    console.log('Connection with database succeeded.');
+db.once('open', function (){
+	db.dropCollection("products", function (err, result) {
+
+        if (err) {
+            console.log("Could not delete existing data");
+        }
+    });
+    console.log('Connection to database.');
 });
 
 module.exports = db;
