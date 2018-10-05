1e7ed2c419d73d058d936d43f59f85409796389b
Rakshita Devurkar <rdevurk1@binghamton.edu>
2018-10-05 12:05:07 -0400

    Added test cases for rendering views

--- test/test.js
+++ test/test.js
@@ -44,3 +44,14 @@ lab.experiment("All API Tests", function() {
 		await server.stop();
 	});
 });
+
+lab.experiment("Rendering views test", function() {
+	lab.test("Renders the Products view successfully", async () => {
+        var products = [{"itemId": 44109840, "name": "Disney - Disney Sleepwear", "price":9.97, "image":"https://i5.walmartimages.com/asr/cde40e3e-3af7-4478-ae9e-3ccd56c57273_1.66232a22191b244da9c6dee5e2538d7a.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF" },
+        				{"itemId": 19716431, "name": "Abu Garcia Black Max Reel", "price":49.96, "image":"https://i5.walmartimages.com/asr/cc9cede8-265a-436f-b855-021e82a5e8be_1.3b50a22e7b9cfab8e637ee5b82204df2.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF" }];
+
+        await server.render('products', {
+			productData: products
+		});
+	});
+});
